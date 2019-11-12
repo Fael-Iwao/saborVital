ecbe14393ec156a96b525dff3d4f6289f87e9c45
Rafael Iwao <rafael.iwao@gmail.com>
2019-11-11 22:24:18 -0300

    Development functions and page forwarding

--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
+var products = [
+    {
+        
+        'photos':['img/products/prod002.jpg','img/products/prod002.jpg'],
+        'name':'Grãos',
+        'shortDescription': 'Grande variedade de grãos de alta qualidade.',
+        'description':'Etiam ut metus et ipsum aliquet consectetur et ac augue. Nulla facilisi. Vivamus eget consectetur diam. Phasellus auctor dignissim turpis id fermentum. Ut pretium eget nisl laoreet scelerisque.',
+        'codProduct':' SV#021',
+        'value': 'Consulte',
+        'rate': '4,5',
+        'category':'grãos'
+
+        
+    },
+    {
+        
+        'photos':['img/products/prod003.jpg'],
+        'name':'Pergola',
+        'shortDescription': 'vinhos e sucos integrais Pergola.',
+        'description':'Etiam ut metus et ipsum aliquet consectetur et ac augue. Nulla facilisi. Vivamus eget consectetur diam. Phasellus auctor dignissim turpis id fermentum. Ut pretium eget nisl laoreet scelerisque.',
+        'codProduct':' SV#022',
+        'value': 'Variados',
+        'rate': '4,5',
+        'category':'sucos'
+
+        
+    },
+    {
+        
+        'photos':['img/products/prod004.jpg'],
+        'name':'San Paolo',
+        'shortDescription': 'Rondellis e Gnocchis em variados sabores.',
+        'description':'Etiam ut metus et ipsum aliquet consectetur et ac augue. Nulla facilisi. Vivamus eget consectetur diam. Phasellus auctor dignissim turpis id fermentum. Ut pretium eget nisl laoreet scelerisque.',
+        'codProduct':' SV#023',
+        'value': 'Variados',
+        'rate': '4,5',
+        'category':'sucos'
+
+        
+    },
+]
+
+
 jQuery(document).ready(function() {
-    $(".geral").hide();
-    $("#wines-br").show();
+    var url = window.location.href;
+    var split = url.split('?')
+    if(split.length < 2){
+        $(".geral").hide();
+        $("#wines-br").show();
+        generateCard();
+    }else{
+        id = split[1].split('=');
+
+        generateDetails(id[1]);
+    }
 });
 
 $(window).on("scroll load resize", function () {
@@ -33,6 +85,74 @@ function wineFilter(id){
     $(".geral").hide();
     $("#wines-"+id).show()
 }
-function modalProduto(){
-    $("#modalProduto").modal('show')
+
+
+function generateCard(){
+       
+    var html = '';
+    for(var i = 0; i < products.length; i ++){
+        var prd = products[i]
+        html += '<div class="card-group col-12 col-sm-4 mb-5">';
+        html += '    <div class="card card-style-001" id="imageCard1">';
+        html += '        <div class="card-body p-0" >';
+        html += '            <img src="' + prd.photos[0] + '" alt="" class="card-img">';
+        html += '        </div>';
+        html += '    </div>';
+        html += '    <div class="card card-style-001 bg-sv-4" >';
+        html += '        <div class="card-body text-white">';
+        html += '            <h4 class="card-title">' + prd.name + '</h4>';
+        html += '            <p class="card-text">' + prd.shortDescription + '</p>';
+        html += '        </div>';
+        html += '        <div class="card-footer text-right text-white" >';
+        html += '            <button class="btn btn-outline-light mt-5 btn-sm w-100" onclick="detailsProducts('+ i +')">Detalhes</button>';
+        html += '        </div>';
+        html += '    </div>';
+        html += '</div>';
+    }
+
+        $(".cardsProducts").append(html);
+
 }
+
+
+function detailsProducts(index){
+    window.location.href = 'product-details.html?id='+index;
+}
+
+function generateDetails(id){
+    if( id >( products.length - 1)){
+        $(".404").removeClass('d-none')
+        $(".200").addClass('d-none')
+    }else{
+        $(".404").addClass('d-none')
+        $(".200").removeClass('d-none')
+
+        var prd = products[id];
+        var thumbs = '';
+        var image = '';
+        var details = '';
+            for(var i = 0; i < prd.photos.length; i ++){
+                thumbs += '<div class="zoom-in  my-2">';
+                thumbs += '    <img src="' + prd.photos[i] + '" class="img-thumbnail img-product clicable" alt="">';
+                thumbs += '</div>';
+            }
+        var bigImage = prd.photos[0].split('.');
+            image = '<img src="' + bigImage[0] + '_max.'+bigImage[1]+'" class="img-fluid" alt="">';
+        
+            details += '<h2 class="text-center text-white mt-2 mb-4">' + prd.name + '</h2>';
+            details += '<hr class="bg-white">';
+            details += '<p class="lead text-justify text-white my-3">' + prd.description + '</p>';
+            details += '<hr class="bg-white">';
+            details += '<h4 class="text-left text-white">' + prd.value + '</h4>';
+            details += '<h6 class="text-left text-white lead">' + prd.codProduct + '</h6>';
+        
+            $(".big-image").html(image);
+            $(".thumb-images").html(thumbs);
+            $(".details-product").html(details);
+    }
+    
+
+}
+
+
+
